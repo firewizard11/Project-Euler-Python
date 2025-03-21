51f45c93869d1d9cc2eea262813488bbaa612aa2
bobby <blahblah@gmail>
2025-03-22 00:56:20 +1100

    Improved speed of trial division and prime test functions

--- problems/problem-3/problem_3.py
+++ problems/problem-3/problem_3.py
@@ -4,19 +4,20 @@ import math
 def trial_division_test(n: int) -> bool:
 	root =	math.ceil(math.sqrt(n))
 	
-	for i in range(2, root):
-		if n % i == 0:
+	for i in range(3, root):
+		if i % 2 != 0 and n % i == 0:
 			return False
 	
 	return True
 
 def trial_division(n: int) -> list[int]:
+	root =	math.ceil(math.sqrt(n))
 	factors = []
-	for i in range(2, n):
+	for i in range(2, root):
 		if (n % i == 0) and trial_division_test(i):
 			factors.append(i)
 	return factors
 
 if __name__ == '__main__':
 	n = 600851475143
-	print(max(trial_division(n)))
+	print(trial_division(n)[-1])
